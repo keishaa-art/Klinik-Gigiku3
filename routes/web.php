64a91d9abe1d6d9426dccd5b848cfb0b02776c6d
hacyuu <ranggagaleri60@gmail.com>
2025-09-25 08:22:19 +0700

    menambahkan home dan index layout

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\Dokter\JadwalPraktekController;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/navigasi', function () {

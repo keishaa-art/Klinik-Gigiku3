02206144f3732b2ce1d5bdc16fbdcfd0bd79ab4b
jon103 <altasolit>
2025-09-30 10:24:39 +0700

    jadwal praktek

--- app/Models/JadwalPraktek.php
+++ app/Models/JadwalPraktek.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use App\Models\User;
+use App\Models\Cabang;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class JadwalPraktek extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'jadwal_prakteks';
+    protected $fillable = ['user_id', 'cabang_id', 'tanggal', 'jam'];
+
+    public function dokter()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function cabang(): BelongsTo
+    {
+        return $this->belongsTo(Cabang::class, 'cabang_id');
+    }
 }
